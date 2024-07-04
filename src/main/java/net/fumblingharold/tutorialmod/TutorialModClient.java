41ea16532f7610b6be0e5ac5d1fd48a8fb8c7e22
HH Landau <hhlan@HHs-MacBook-Air.local>
2024-07-04 00:54:05 -0400

    Initial commit

---
+++ src/main/java/net/fumblingharold/tutorialmod/TutorialModClient.java
@@ -0,0 +1,11 @@
+package net.fumblingharold.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.ModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+  @Override
+  public void onInitializeClient() {
+
+  }
+}
---
+++ src/main/java/net/fumblingharold/tutorialmod/TutorialModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.fumblingharold.tutorialmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TutorialModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
