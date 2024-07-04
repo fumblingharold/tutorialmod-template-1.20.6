8c87225bf43d9e625c8f9e8e06dc5a1cd27af886
fumblingharold <fumblingharold@gmail.com>
2024-07-04 01:22:34 -0400

    Added textures and additional item "Raw Ruby"

--- src/main/java/net/fumblingharold/tutorialmod/TutorialMod.java
+++ src/main/java/net/fumblingharold/tutorialmod/TutorialMod.java
@@ -2,6 +2,7 @@ package net.fumblingharold.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fumblingharold.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,7 +12,7 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
 		LOGGER.info("Hello Fabric world!");
 	}
 }
