c20682260aee418b3551ee7def991ac6298e032e
fumblingharold <fumblingharold@gmail.com>
2024-07-04 01:41:17 -0400

    Added item group for new items

--- src/main/java/net/fumblingharold/tutorialmod/TutorialMod.java
+++ src/main/java/net/fumblingharold/tutorialmod/TutorialMod.java
@@ -2,6 +2,7 @@ package net.fumblingharold.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fumblingharold.tutorialmod.item.ModItemGroups;
 import net.fumblingharold.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,6 +13,7 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		ModItemGroups.registerItemGroups();
 		ModItems.registerModItems();
 		LOGGER.info("Hello Fabric world!");
 	}
---
+++ src/main/java/net/fumblingharold/tutorialmod/item/ModItemGroups.java
@@ -0,0 +1,25 @@
+package net.fumblingharold.tutorialmod.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.fumblingharold.tutorialmod.TutorialMod;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+
+public class ModItemGroups {
+
+  public static final ItemGroup RUBY_GROUP = Registry.register(Registries.ITEM_GROUP,
+      new Identifier(TutorialMod.MOD_ID, "ruby"),
+      FabricItemGroup.builder().displayName(Text.translatable("itemgroup.ruby"))
+          .icon(() -> new ItemStack(ModItems.RUBY)).entries((displayContext, entries) -> {
+            entries.add(new ItemStack(ModItems.RUBY));
+            entries.add(new ItemStack(ModItems.RAW_RUBY));
+          }).build());
+
+  public static void registerItemGroups () {
+    TutorialMod.LOGGER.info("Registering Item Groups for " + TutorialMod.MOD_ID);
+  }
+}
